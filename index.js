59f160b2b8d0833e56ea6e9ce66357a00df6c486
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-12-19 11:11:46 +0600

    added vercel json file

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("SchollerStream");
     const usersCollection = db.collection("users");
@@ -892,10 +892,10 @@ async function run() {
       }
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
   }
 }
